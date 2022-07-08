0af80df045bfbe204d9c97aa17a499f5b6f6cbfd
Ivan Todorov <ivan.todorov@progress.com>
2022-07-08 14:30:00 +0300

    Create storage provider

---
+++ Source/Services/IStorageProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace Telerik.Project.Management.Services;
+
+public interface IStorageProvider
+{
+    string Url(string filePath);
+    bool Exists(string filePath);
+    Stream Create(string filePath);
+    Stream Open(string filePath);
+    void Delete(string filePath);
+}
---
+++ Source/Services/StorageProvider.cs
@@ -0,0 +1,59 @@
+﻿namespace Telerik.Project.Management.Services;
+
+public class StorageProvider : IStorageProvider
+{
+    private const string directoryName = "Data";
+
+    private readonly string basePath;
+
+    public StorageProvider(IWebHostEnvironment hostEnvironment)
+    {
+        var rootPath = hostEnvironment.WebRootPath;
+
+        this.basePath = Path.Combine(rootPath, directoryName);
+    }
+
+    public string Url(string filePath)
+    {
+        var relativePath = Path.Combine(directoryName, filePath);
+
+        return relativePath.Replace('\\', '/');
+    }
+
+    public bool Exists(string filePath)
+    {
+        var absolutePath = Path.Combine(this.basePath, filePath);
+
+        return File.Exists(absolutePath);
+    }
+
+    public Stream Create(string filePath)
+    {
+        var absolutePath = Path.Combine(this.basePath, filePath);
+        var directoryPath = Path.GetDirectoryName(absolutePath);
+
+        if (directoryPath is not null && !Directory.Exists(directoryPath))
+        {
+            Directory.CreateDirectory(directoryPath);
+        }
+
+        return File.Create(absolutePath);
+    }
+
+    public Stream Open(string filePath)
+    {
+        var absolutePath = Path.Combine(this.basePath, filePath);
+
+        return File.OpenRead(absolutePath);
+    }
+
+    public void Delete(string filePath)
+    {
+        var absolutePath = Path.Combine(this.basePath, filePath);
+
+        if (File.Exists(absolutePath))
+        {
+            File.Delete(absolutePath);
+        }
+    }
+}
