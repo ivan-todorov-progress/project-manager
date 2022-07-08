b0a8f77e4fd70c8d4288821e07fbe082a60445f5
Ivan Todorov <ivan.todorov@progress.com>
2022-07-08 15:54:15 +0300

    Create history service

---
+++ Source/Services/HistoryService.cs
@@ -0,0 +1,18 @@
+﻿using Telerik.Project.Management.Models;
+
+namespace Telerik.Project.Management.Services;
+
+public class HistoryService : IHistoryService
+{
+    private readonly IStorageService storageService;
+
+    public HistoryService(IStorageService storageService)
+    {
+        this.storageService = storageService;
+    }
+
+    public async Task<IEnumerable<ChangeSet>> GetChangesAsync(string parentId)
+    {
+        return await this.storageService.LoadChangesAsync(parentId);
+    }
+}
---
+++ Source/Services/IHistoryService.cs
@@ -0,0 +1,8 @@
+﻿using Telerik.Project.Management.Models;
+
+namespace Telerik.Project.Management.Services;
+
+public interface IHistoryService
+{
+    Task<IEnumerable<ChangeSet>> GetChangesAsync(string parentId);
+}
