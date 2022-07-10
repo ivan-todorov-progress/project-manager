b61c55cd927201330b85031f384c2924b28d8211
Ivan Todorov <ivan.todorov@progress.com>
2022-07-10 15:13:35 +0300

    Create history controller

---
+++ Source/Controllers/HistoryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Telerik.Project.Management.Models;
+using Telerik.Project.Management.Services;
+
+namespace Telerik.Project.Management.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HistoryController : ControllerBase
+    {
+        private readonly IHistoryService historyService;
+
+        public HistoryController(IHistoryService historyService)
+        {
+            this.historyService = historyService;
+        }
+
+        [HttpGet("{parentId}")]
+        public async Task<IEnumerable<ChangeSet>> GetChangesAsync(string parentId)
+        {
+            return await this.historyService.GetChangesAsync(parentId);
+        }
+    }
+}
