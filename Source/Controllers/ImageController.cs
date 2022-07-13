0f52f8d2aaa3312473fa3ee0fcd32b9b19d2003f
Ivan Todorov <ivan.todorov@progress.com>
2022-07-13 12:12:36 +0300

    Create image controller

---
+++ Source/Controllers/ImageController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Telerik.Project.Management.Models;
+using Telerik.Project.Management.Services;
+
+namespace Telerik.Project.Management.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ImageController : ControllerBase
+    {
+        private readonly IImageService imageService;
+
+        public ImageController(IImageService imageService)
+        {
+            this.imageService = imageService;
+        }
+
+        [HttpPost("{projectId}")]
+        public async Task<ImageInfo> UploadImageAsync(string projectId)
+        {
+            var imageFiles = Request.Form.Files;
+            var imageFile = imageFiles.GetFile("Image");
+
+            if (imageFile is null)
+            {
+                return new ImageInfo();
+            }
+
+            return await this.imageService.SaveImageAsync(projectId, imageFile);
+        }
+    }
+}
