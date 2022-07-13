4618202c0c9ae81cb7690159e2b62ba3a317e546
Ivan Todorov <ivan.todorov@progress.com>
2022-07-13 11:36:37 +0300

    Create image service

---
+++ Source/Services/IImageService.cs
@@ -0,0 +1,8 @@
+﻿using Telerik.Project.Management.Models;
+
+namespace Telerik.Project.Management.Services;
+
+public interface IImageService
+{
+    Task<ImageInfo> SaveImageAsync(string projectId, IFormFile imageFile);
+}
---
+++ Source/Services/ImageService.cs
@@ -0,0 +1,29 @@
+﻿using Telerik.Project.Management.Models;
+
+namespace Telerik.Project.Management.Services;
+
+public class ImageService : IImageService
+{
+    private readonly IStorageProvider storageProvider;
+
+    public ImageService(IStorageProvider storageProvider)
+    {
+        this.storageProvider = storageProvider;
+    }
+
+    public async Task<ImageInfo> SaveImageAsync(string projectId, IFormFile imageFile)
+    {
+        var imageName = imageFile.FileName;
+        var imagePath = Path.Combine(projectId, imageName);
+        var imageUrl = this.storageProvider.Url(imagePath);
+        using var imageStream = this.storageProvider.Create(imagePath);
+
+        await imageFile.CopyToAsync(imageStream);
+
+        return new ImageInfo
+        {
+            Name = imageName,
+            Url = imageUrl
+        };
+    }
+}
